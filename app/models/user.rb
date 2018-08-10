5d9adf06ff7ca2d962198c8fe53d01cc4fe9fe54
MATEFORLIFE <RODRIGUEZ.RUIZ.ALEX@GMAIL.COM>
2018-08-10 13:04:11 -0400

    integrate rspec and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable,
          omniauth_providers: [:facebook]
 
+  validates :username, presence: true, length: { in: 3..20 }
+  validates_uniqueness_of :username
+  validate :username_regex
+
   def self.from_omniauth(auth)
     where(provider: auth['provider'], uid: auth['uid']).first_or_create do |user|
       user.email = auth['info']['email']
       user.name = auth['info']['name']
       user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  private
+
+  def username_regex
+    unless username =~ /\A[a-zA-Z]*[a-zA-Z][a-zA-Z0-9_]*\z/
+      errors.add(:username, 'el nombre de usuario debe iniciar con una letra,
+                             puede contener a demas números y guión bajo')
     end
   end
 end
